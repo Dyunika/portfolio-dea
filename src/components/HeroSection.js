cada2b754735ecd59a5455f55641d763a7b4c26e
zhanpar789 <zhanfar.isman@gmail.com>
2022-02-08 12:07:19 +0700

    pbocommit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,10 +3,13 @@ import Navbar from "./Navbar";
 function HeroSection() {
     return (
         <div className="bg-cyan-600 h-[550px]">
-            <div className="container mx-auto"></div>
+            <div className="container mx-auto">
             <Navbar/>
-            <h1 className="text-white text-center text-3xl w-6/12 mx-auto leading-relaxed font-semibold font-mono">Saya seorang front-end engineer, back-end engineer, dan juga UI designer</h1>
-            
+               <div className="text-center mt-12">
+                 <h1 className="text-white text-center text-3xl w-6/12 mx-auto leading-relaxed font-semibold font-mono">Saya seorang front-end engineer, back-end engineer, dan juga UI designer</h1>
+                  <p ></p>
+                </div> 
+            </div>         
         </div>
     );
 }
